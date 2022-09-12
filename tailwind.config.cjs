27b7c8b5359664edde7853ca5c3aed1b4ecf9d03
guitischer <guitischer@hotmail.com.br>
2022-09-12 20:30:03 -0300

    finished landing page boiler plate

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,10 +5,15 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
-        galaxy: "url('/background-galaxy.png')"
-      }
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(90deg, #9572FC 0%, #43E7AD 70%, #E1D55D 100%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+      },
     },
   },
   plugins: [],
