68962739497e70daf1807b0ac9da136811443a97
guitischer <guitischer@hotmail.com.br>
2022-09-12 19:38:24 -0300

    added assets, tailwind, bg and nlw logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import './styles/main.css'
+
+import logoImg from './assets/logo-nlw-esports.svg'
+
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <div className='max-w-[1344] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt="" />
+    </div>)
 }
 
 export default App
