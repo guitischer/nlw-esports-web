3ad8875b3cb9907acce46e19521170d8b689f31e
guitischer <guitischer@hotmail.com.br>
2022-09-14 23:26:59 -0300

    formated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './styles/main.css'
-import GameBanner from './components/GameBanner';
-import CreateAdBanner from './components/CreateAdBanner';
+import "./styles/main.css";
+import GameBanner from "./components/GameBanner";
+import CreateAdBanner from "./components/CreateAdBanner";
 
-import logoImg from './assets/logo-nlw-esports.svg'
-import { useEffect, useState } from 'react';
+import logoImg from "./assets/logo-nlw-esports.svg";
+import { useEffect, useState } from "react";
 
 interface Game {
   id: string;
@@ -11,41 +11,46 @@ interface Game {
   bannerUrl: string;
   _count: {
     ads: number;
-  }
+  };
 }
 
 function App() {
   const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:3333/games')
-      .then(response => response.json())
-      .then(data => {
-        setGames(data)
-      })
-  }, [])
+    fetch("http://localhost:3333/games")
+      .then((response) => response.json())
+      .then((data) => {
+        setGames(data);
+      });
+  }, []);
 
   return (
-    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
 
-      <h1 className='text-6xl text-white font-black mt-20'>
-        Seu <span className='bg-nlw-gradient bg-clip-text text-transparent'>duo</span> está aqui.
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="bg-nlw-gradient bg-clip-text text-transparent">
+          duo
+        </span>{" "}
+        está aqui.
       </h1>
 
-      <div className='grid grid-cols-6 gap-6 mt-16'>
-        {games.map(game => (
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        {games.map((game) => (
           <GameBanner
             key={game.id}
             bannerUrl={game.bannerUrl}
             title={game.title}
-            adsCount={game._count.ads} />
+            adsCount={game._count.ads}
+          />
         ))}
       </div>
 
       <CreateAdBanner />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
